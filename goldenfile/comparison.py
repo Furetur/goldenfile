d651e18cf7acba7d8cabc44250f711baefeeac40
Egor Bronnikov <bronnikov.40@mail.ru>
2023-11-14 02:21:47 +0300

    Add basic implementation of `cmp_file()` function

--- goldenfile/comparison.py
+++ goldenfile/comparison.py
@@ -3,6 +3,10 @@
 
 from typing import Callable, Any
 from difflib import context_diff
+from filecmp import cmp
+from pathlib import Path
+
+from .exception import GoldenfileError
 
 
 def _default_str_comparator(lhs: str, rhs: str) -> bool:
@@ -13,7 +17,7 @@ def cmp_str(
     lhs: str,
     rhs: str,
     *,
-    comparator: Callable[[str, str], Any] = _default_str_comparator
+    comparator: Callable[[str, str], Any] = _default_str_comparator,
 ) -> Any:
     return comparator(lhs, rhs)
 
@@ -30,3 +34,13 @@ def diff_str(lhs: str, rhs: str) -> str:
         )
 
     return "\n".join(context_diff(lhs, rhs, fromfile="expect", tofile="actual"))
+
+
+def cmp_file(lhs: Path, rhs: Path):
+    if lhs.is_file():
+        raise GoldenfileError(f"Incorrect file: {lhs}")
+
+    if rhs.is_file():
+        raise GoldenfileError(f"Incorrect file: {lhs}")
+
+    return cmp(lhs, rhs, shallow=False)
