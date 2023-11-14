28105d247341f99def4c61beebe41454141aba10
Egor Bronnikov <bronnikov.40@mail.ru>
2023-11-14 12:55:23 +0300

    Add basic implementation of "cmp_dir()" function

--- goldenfile/comparison.py
+++ goldenfile/comparison.py
@@ -3,7 +3,7 @@
 
 from typing import Callable, Any, Iterator
 from difflib import context_diff, unified_diff
-from filecmp import cmp
+from filecmp import cmp, dircmp
 from pathlib import Path
 
 from .exception import GoldenfileError
@@ -67,8 +67,35 @@ def diff_file(lhs: Path, rhs: Path) -> str:
     return "\n".join(difference)
 
 
+def _cmp_dir(lhs: Path, rhs: Path) -> bool:
+    difference: dircmp = dircmp(lhs, rhs)
+
+    if difference.left_only or difference.right_only or difference.diff_files:
+        return False
+
+    for subdir in difference.common_dirs:
+        lhs_subdir: Path = Path(lhs, subdir)
+        rhs_subdir: Path = Path(rhs, subdir)
+        if not _cmp_dir(lhs_subdir, rhs_subdir):
+            return False
+
+    return True
+
+
 def cmp_dir(lhs: Path, rhs: Path, shallow: bool = True) -> bool:
-    raise NotImplementedError()
+    if not lhs.is_dir():
+        raise GoldenfileError(f"Incorrect directory: {lhs}")
+
+    if not rhs.is_dir():
+        raise GoldenfileError(f"Incorrect directory: {rhs}")
+
+    # NOTE: Recursively traversing all subfoldres of a target directories
+    #       and compare them.
+    if not shallow:
+        return _cmp_dir(lhs, rhs)
+
+    difference: dircmp = dircmp(lhs, rhs)
+    return difference.left_only or difference.right_only or difference.diff_files
 
 
 def diff_dir(lhs: Path, rhs: Path, shallow: bool = True) -> bool:
