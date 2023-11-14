0a1b9ab08be75a088f2c54627505aa70f5775a0d
Egor Porsev <furetur@gmail.com>
2023-11-14 19:21:23 +0300

    Discover tests (#3)
    add discover

---
+++ goldenfile/discover.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+from typing import Callable, Sequence, Optional
+from dataclasses import dataclass
+import yaml
+
+from goldenfile.types import Tag, TagFilter, Test
+
+INPUT_SUFFIX = ".input"
+STDOUT_SUFFIX = ".stdout"
+STDERR_SUFFIX = ".stderr"
+GENERATED_FILE_SUFFIX = ".gen"
+
+
+def read_test(test_dir: Path) -> Test:
+    test_name = test_dir.stem
+
+    def read_tags() -> Sequence[Tag]:
+        with open(test_dir / (test_name + ".config.yaml")) as file:
+            return yaml.safe_load(file)["tags"]
+
+    def get_file(suffix: str) -> Optional[Path]:
+        matching_files = [p for p in test_dir.iterdir() if suffix in p.suffixes]
+        if len(matching_files) == 0:
+            return None
+        else:
+            return matching_files[0]
+
+    return Test(
+        name=test_name,
+        tags=read_tags(),
+        input=get_file(INPUT_SUFFIX),
+        golden_stdout=get_file(STDOUT_SUFFIX),
+        golden_stderr=get_file(STDERR_SUFFIX),
+        golden_generated_file=get_file(GENERATED_FILE_SUFFIX)
+    )
+
+def discover(test_suite_dir: Path, tag_filter: TagFilter = lambda _: True) -> Sequence[Test]:
+    test_dirs = (p for p in test_suite_dir.iterdir() if p.is_dir())
+    tests = (read_test(test_dir) for test_dir in test_dirs)
+    return [t for t in tests if tag_filter(t.tags)]
