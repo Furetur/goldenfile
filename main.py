9ac552b43a9421d1c675533e631fd6d889249269
Kira Prokopenko <prokopenko.kira2@huawei-partners.com>
2023-11-15 11:30:51 +0300

    add exporting json results & replacing current files for failed tests

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ from typing import Sequence
 from goldenfile.checker import bytewise_checker
 from goldenfile.reporters.html_reporter import HtmlDiffReporter
 from goldenfile.reporters.unify_diff import UnifyDiffReporter
+from goldenfile.reporters.save_report import save_report
+from goldenfile.reporters.replace_golden import ReplaceGoldenReporter
 from goldenfile.runner import ShellCommand, shell_runner
 from goldenfile.test_discoverers.simple_discoverer import simple_discoverer
 from goldenfile.pipeline import Pipeline
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-r", "--replace", dest="replace", action="store_true", default=False)
+(options, args) = parser.parse_args()
+assert len(args) == 0
+
+should_replace = [ReplaceGoldenReporter] if options.replace else []
 
 
 class Python3ShellCommand(ShellCommand):
@@ -17,7 +27,7 @@ pipeline = Pipeline(
     test_discoverer=simple_discoverer,
     runner=shell_runner(Python3ShellCommand()),
     checker=bytewise_checker,
-    reporters=[UnifyDiffReporter, HtmlDiffReporter]
+    reporters=[UnifyDiffReporter, HtmlDiffReporter, save_report(Path("temp/report.json"))] + should_replace
 )
 
 if __name__ == '__main__':
